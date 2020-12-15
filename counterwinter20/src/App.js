f8282c2a231d32fbb5ef5565228c13a973d15935
Mel Fabella <mf820am@gmail.com>
2020-12-14 19:05:58 -0800

    end counters

---
+++ counterwinter20/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import Counter from './components/counters/Counter';
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { counterValue: 0 }
+  //   this.inc = this.inc.bind(this)
+  // }
+  // state = { counterValue: 0 }
+  // inc() {
+  //   this.state.counterValue++
+  // }
+  // inc = () => {
+  //   this.setState({ counterValue: this.state.counterValue + 1 })
+  // }
+  // dec = () => {
+  //   this.setState({ counterValue: this.state.counterValue - 1 })
+  // }
+  // dec = () => {
+  //   this.setState( (state) => {
+  //     return { counterValue: state.counterValue -1 }
+  //   })
+  // }
+  // dec = () => {
+  //   const { counterValue } = this.state
+  //   this.setState({ counterValue: counterValue -1 })
+  // }
+  render() {
+    return (
+      <>
+        <h1>Counter Example</h1>
+        {/* <p>
+          {this.state.counterValue}
+        </p>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button> */}
+        <Counter colorName="red" />
+        <Counter colorName="purple" />
+        <Counter colorName="green" />
+      </>
+    )
+  }
+}
+export default App;
