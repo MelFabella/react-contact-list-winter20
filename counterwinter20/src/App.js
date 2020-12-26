074b408c1c29aa99f67a1dc43efae2b9d3f2e288
Mel Fabella <mf820am@gmail.com>
2020-12-26 15:40:35 -0800

    update notes

--- counterwinter20/src/App.js
+++ counterwinter20/src/App.js
@@ -2,31 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import Counter from './components/counters/Counter';
+
 class App extends Component {
   // constructor(props) {
   //   super(props);
   //   this.state = { counterValue: 0 }
   //   this.inc = this.inc.bind(this)
-  // }
-  // state = { counterValue: 0 }
+  // } OLD SYNTAX
+  // state = { counterValue: 0 } NEW SYNTAX
+  
   // inc() {
   //   this.state.counterValue++
-  // }
+  // } OLD SYNTAX
+  
   // inc = () => {
   //   this.setState({ counterValue: this.state.counterValue + 1 })
-  // }
+  // } NEW SYNTAX
+
   // dec = () => {
   //   this.setState({ counterValue: this.state.counterValue - 1 })
   // }
+
   // dec = () => {
   //   this.setState( (state) => {
   //     return { counterValue: state.counterValue -1 }
   //   })
-  // }
+  // } DRY SYNTAX
+
   // dec = () => {
   //   const { counterValue } = this.state
   //   this.setState({ counterValue: counterValue -1 })
-  // }
+  // } DRY SYNTAX HENRY'S PREP
+
   render() {
     return (
       <>
@@ -43,4 +50,5 @@ class App extends Component {
     )
   }
 }
+
 export default App;
